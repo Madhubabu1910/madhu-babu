8fea24a16f077a8d57ca5c3f017ba1bcc37abf4b
Madhu Babu <102810327+Madhubabu1910@users.noreply.github.com>
2022-09-08 16:41:22 +0530

    jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+yum install git -y &&
+yum install wget -y &&
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &&
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key &&
+yum install fontconfig java-11-openjdk -y &&
+yum install jenkins -y &&
+service jenkins start &&
+sytemctl enable jenkins 
+
